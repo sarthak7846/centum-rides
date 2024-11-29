d8b6263ea5b0b332daa22b61799314a9f985db48
Sarthak Kumar Behera <sarthakbehera09@gmail.com>
2024-11-29 21:29:13 +0530

    Added db functionality for adding new users

---
+++ app/(api)/user+api.ts
@@ -0,0 +1,23 @@
+import { neon } from "@neondatabase/serverless";
+
+export async function POST(request: Request) {
+  try {
+    const sql = neon(`${process.env.DATABASE_URL}`);
+
+    const { name, email, clerkId } = await request.json();
+
+    if (!name || !email || !clerkId) {
+      return Response.json(
+        { error: "Missing required fields" },
+        { status: 400 }
+      );
+    }
+
+    const response =
+      await sql`INSERT INTO users(name, email, clerk_id) values (${name}, ${email}, ${clerkId})`;
+    return new Response(JSON.stringify({ data: response, status: 201 }));
+  } catch (err) {
+    console.log(err);
+    return Response.json({ error: err }, { status: 500 });
+  }
+}
